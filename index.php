930c03c9bf57ad654e989a06c3041555639a4580
drilify <drilify.tdm@gmail.com>
2017-05-30 20:55:19 -0300

    teste

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+</head>
+<body>
+<?php 
+	include './model/Conexao.php';
+	var_dump(Conexao::getConexao());
+ ?>
+</body>
+</html>
